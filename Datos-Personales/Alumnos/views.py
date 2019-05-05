d4f706bc5aef01fe097bc8a5ba43426105102abb
Marabo98 <44044012+Marabo98@users.noreply.github.com>
2019-05-05 17:18:17 -0500

    Add files via upload

---
+++ Datos-Personales/Alumnos/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import DatosPersonales
+
+
+
+def index(request):
+    return render(request,"Alumnos/index.html")
+
+def res(request):
+    Datos=DatosPersonales.objects.all()
+    return render(request, "Alumnos/resultado.html",{"Alumnos":Datos})
+
+
+
